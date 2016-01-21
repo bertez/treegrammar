34ae96983e1d7b92becf9bf4414925686e3f3a09
Berto Yáñez <berto@ber.to>
2016-01-21 23:54:40 +0100

    Finished tests

--- test/test.js
+++ test/test.js
@@ -16,17 +16,6 @@ const rules = {
     ]
 }
 
-const generated = [
-    'the house is green',
-    'the house is big',
-    'the cat is green',
-    'the cat is big',
-    'A green house',
-    'A big house',
-    'A green cat',
-    'A big cat'
-];
-
 describe('TreeGrammar', () => {
     describe('Instantiating', () => {
         it('should not throw an error when instantiating without arguments', () => {
@@ -61,6 +50,17 @@ describe('TreeGrammar', () => {
         it('should generate string according the rules', () => {
             const g = new TreeGrammar(rules);
 
+            const generated = [
+                'the house is green',
+                'the house is big',
+                'the cat is green',
+                'the cat is big',
+                'A green house',
+                'A big house',
+                'A green cat',
+                'A big cat'
+            ];
+
             expect(generated.indexOf(g.generate())).to.be.at.least(0);
             expect(generated.indexOf(g.generate())).to.be.at.least(0);
             expect(generated.indexOf(g.generate())).to.be.at.least(0);
@@ -69,20 +69,67 @@ describe('TreeGrammar', () => {
         });
 
         it('should start in a different rule than <start>', () => {
+            const g = new TreeGrammar(rules);
 
+            expect(rules['<adj>'].indexOf(g.generate('<adj>'))).to.be.at.least(0);
+        });
+
+        it('should throw an error if the grammar is empty', () => {
+            expect(() => new TreeGrammar().generate()).to.throw(Error);
         });
     });
 
     describe('API', () => {
         it('should add rules', () => {
+            const g = new TreeGrammar();
 
+            g.addRule('<start>', ['<template>'])
+            .addRule('<template>', ['foo']);
+
+            expect(g.generate()).to.equal('foo');
+        });
+
+        it('should throw error if the rule name is not well formed', () => {
+            const g = new TreeGrammar();
+
+            expect(() => g.addRule('foo', 'bar')).to.throw(Error);
+        });
+
+        it('should throw error if the rule content is not an array', () => {
+            const g = new TreeGrammar();
+
+            expect(() => g.addRule('<foo>', 'bar')).to.throw(Error);
         });
 
         it('should overwrite rules', () => {
+            const g = new TreeGrammar();
+
+            g.addRule('<start>', ['<template>'])
+            .addRule('<template>', ['foo'])
+            .addRule('<template>', ['bar'], true);
+
+            expect(g.generate()).to.equal('bar');
+        });
+
+        it('should throw an error adding a existent rule without the overwrite flag', () => {
+            const g = new TreeGrammar();
+
+            g.addRule('<start>', ['<template>'])
+            .addRule('<template>', ['foo']);
+
+            expect(() => g.addRule('<template>', ['bar'])).to.throw(Error);
 
         });
 
         it('should delete rules', () => {
+            const g = new TreeGrammar();
+
+            g.addRule('<start>', ['<template>'])
+            .addRule('<template>', ['foo'])
+            .deleteRule('<template>')
+            .addRule('<template>', ['bar']);
+
+            expect(g.generate()).to.equal('bar');
 
         });
     });
